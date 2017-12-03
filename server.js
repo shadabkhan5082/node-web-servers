3c934a7cd6b8d368e4abfa0b0056d2ed3b862d91
shadabkhan5082 <shadabkhan5082@gmail.com>
2017-12-03 16:26:20 -0600

    setup start and heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express=require('express');
 const hbs= require('hbs');
 const fs= require('fs');
 
-
+const port= process.env.PORT || 3000;
 const app= express();
 
 app.set('view source', 'hbs');
@@ -21,9 +21,9 @@ app.use((req,res,next)=>{
  next();
 });
 
-app.use((req,res)=>{
-  res.render('maintenence.hbs')
-});
+// app.use((req,res)=>{
+//   res.render('maintenence.hbs')
+// });
 
 app.use(express.static(__dirname+'/public'));
 
@@ -58,4 +58,6 @@ app.get('/about',(req,res)=>{
 });
 
 
-app.listen(3000);
+app.listen(port,()=>{
+  console.log(`server is running on :${port} `)
+});

56dc4a9a8ed31613677100a258828815b9f9a804
shadabkhan5082 <shadabkhan5082@gmail.com>
2017-12-03 12:12:49 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express=require('express');
+const hbs= require('hbs');
+const fs= require('fs');
+
+
+const app= express();
+
+app.set('view source', 'hbs');
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+
+
+
+app.use((req,res,next)=>{
+  var now= new Date().toString();
+  var a=`${now}:${req.method} ${req.path}`;
+  //console.log(a);
+  fs.appendFileSync('server.log',a+'\n');
+
+ next();
+});
+
+app.use((req,res)=>{
+  res.render('maintenence.hbs')
+});
+
+app.use(express.static(__dirname+'/public'));
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle: 'Home Page',
+    welcomeMessage: "Welcome to my website",
+    currentYear: new Date().getFullYear()
+  })
+});
+
+app.get('/bad', (req,res)=>{
+  res.send({
+    Error:'this is an error'
+  });
+});
+
+app.get('/about',(req,res)=>{
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
+});
+
+
+app.listen(3000);
